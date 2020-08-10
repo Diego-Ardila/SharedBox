46415ea6886dd26c15645ee396805eafeddedc81
Diego <dardila90@gmail.com>
2020-08-10 11:45:40 -0500

    adding mocks

---
+++ src/mock.js/mock.js
@@ -0,0 +1,22 @@
+let lender=[{}]
+
+
+let tenant=[{}]
+
+let spaces=[{}]
+
+let spacesTags=[{}]
+
+let notifications=[{}]
+
+let inventory=[{}]
+
+let scores=[{}]
+
+let chats=[{}]
+
+let events=[{}]
+
+let productsElementsObjects=[{}]
+
+let scoresCriterias=[{}]
