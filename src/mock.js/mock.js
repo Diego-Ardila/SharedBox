2864c3a3447dab0a43c8820575f64d0a6d7e71c8
Diego-Martinez <d.ricardo2martinez@gmail.com>
2020-08-10 16:36:38 -0500

    update from event, productsElementObjects, scoreCriterals

--- src/mock.js/mock.js
+++ src/mock.js/mock.js
@@ -15,8 +15,79 @@ let scores=[{}]
 
 let chats=[{}]
 
-let events=[{}]
+let events=[
+    {
+        id: 1,
+        event:"event 1",
+        id_notifications : [{
+            id_notificatio : 1,    
+            id_notificatio : 3,    
+        }]
+    },
+ 
+    {
+        id: 2,
+        event:"event 2",
+        id_notifications : [{
+            id_notificatio : 2,    
+            id_notificatio : 4
+        }],
+    },
+    {
+        id: 3,
+        event:"event 3",
+        id_notifications : [{
+            id_notificatio : 1,
+            id_notificatio : 2,
+            id_notificatio : 3,
+            id_notificatio : 4
+        }]
+    },
+]
 
-let productsElementsObjects=[{}]
+let productsElementsObjects=[
+    {
+        id:1,
+        object:"television",
+        category:"electronic",
+        units:1
+    },
+    {
+        id:2,
+        object:"fruts",
+        category:"organic",
+        units:10
+    },
+    {
+        id:3,
+        object:"steel sheets",
+        category:"metal",
+        units:4
+    },
+    {
+        id:4,
+        object:"wood sheets",
+        category:"organic",
+        units:30
+    },
 
-let scoresCriterias=[{}]
+]
+
+let scoresCriterias=[
+    {
+        id: 1,
+        score :"organic",
+    },
+    {
+        id:2,
+        score:"plastic"
+    },
+    {
+        id:3,
+        score:"chemical"
+    },
+    {
+        id:4,
+        score:"metal"
+    }
+]
