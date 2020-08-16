ea3532a31ab4f98c9c65aa71e454ad4ce63562da
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-08-16 17:56:32 -0500

    added some UX to the login fields and routing functionality to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 import Login from "./pages/Login"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 
 
 function App() {
+  //const login = <Login></Login>
   return (
     <Router>
     <div className="App">
-      <Login></Login>
+      <Switch>
+        <Route exact path="/lender/login" component={Login} />
+        {/* <Route exact path="/lender/register/" component={register} /> */}
+        {/* <Route exact path="/lender/profile/:id" component={Something} /> */}
+        {/* <Route path="*" component={Default} /> */}
+        <Redirect from="*" to="/lender/login" />
+      </Switch>
     </div>
     </Router>
   );
