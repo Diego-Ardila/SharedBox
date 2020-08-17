ed8627c290b56245f16a3c7a5f26281c6ee1083b
Diego <dardila90@gmail.com>
2020-08-16 19:34:22 -0500

    adding git ignore .vscode

---
+++ src/utils/validators.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let registerValidator = (state) => {
+    const {errors, ...values} = state;
+    if(values.password !== values.v_password){
+        
+    }
+
+}
+
+export default registerValidator;
