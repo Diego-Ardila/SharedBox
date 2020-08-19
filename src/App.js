d0cc4f16f03da69c5b8f7dd9e61f1389a31b88c1
Veilhelm Alexander <me@veilhelmalexander.com>
2020-08-19 12:08:38 -0500

    added styled-components to login and resolved private routing (#11)
    Co-authored-by: veilhelmAlex <veilhelm.alexander@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect, useHistory } from "react-router-dom";
 import './App.css';
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
-import register from "../src/pages/register"
+import register from "./pages/register"
+
+function PrivateRoute(props) {
+const history = useHistory()
+
+useEffect(() => {
+  const token = localStorage.getItem("token")
+  const isValid = true // an axios called to verify the token is required
+  if(!token && isValid) history.push("/lender/login")
+}, [])
+
+return(
+  <Route {...props}></Route>
+)
+}
 
 function App() {
   return (
@@ -12,7 +26,7 @@ function App() {
         <Switch>
           <Route exact path="/lender/login" component={Login} />
           <Route exact path="/lender/register/" component={register} />
-          <Route exact path="/lender/profile" component={Profile} />
+          <PrivateRoute exact path="/lender/profile" component={Profile} />
           <Redirect from="*" to="/lender/login" />
         </Switch>
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,30 @@
 import React from "react"
 import LoginForm from "../components/login/LoginForm"
+import styled from "styled-components"
+import Logo from "../logo.svg"
 
+
+const MainWraper = styled.section`
+    background: linear-gradient(180deg, #FFF9F4 1.12%, #B0CAC7 100%);
+    height: 100vh;
+    margin: 0 auto;
+    border: 0;
+    font-family: Montserrat;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 24px;
+    `
+const NewUserButton = styled.button` 
+    background: none;
+    opacity: 1;
+    line-height: 24px;
+    text-align: center;
+    text-decoration-line: underline;
+    color: #001244;
+    border: none;
+`
 class Login extends React.Component{
     state = {
         loginError : false,
@@ -11,18 +35,28 @@ class Login extends React.Component{
         this.setState({loginError : true, errMessage : err})
     }
 
+    redirect = () => {
+        this.props.history.push("/lender/register/")
+    }
+
     render() {
         return(
+            <MainWraper>
             <div className = "login-page">
                 <div>
                     <p>LOGIN HEADER (TO DO)</p>
-
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <img src={Logo} alt="logo"></img>
                     <LoginForm handleErrorLogin = {this.handleErrorLogin} history = {this.props.history}></LoginForm>
                 </div>
-                <button>new user? create new account</button><br></br>
+                <br></br>
+                <NewUserButton onClick={this.redirect}>new user? create new account</NewUserButton><br></br>
                 <p>{this.state.loginError ? this.state.errMessage : ""}</p>
                 <p>FOOTER (TO DO)</p>
             </div>
+            </MainWraper>
         )
     }
 }
