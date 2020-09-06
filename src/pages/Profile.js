eeac80304dda02d441df3d36485e57cfad65505a
Diego-Martinez <d.ricardo2martinez@gmail.com>
2020-09-06 15:41:20 -0500

    correct error profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -12,9 +12,9 @@ class Profile extends Component{
     render(){
         return(
             <Container>
-                <h3>{this.state.error}</h3>
+                <h3></h3>
                 <ProfileForm />
-                {this.state.typeUser === "lender" ?
+                {localStorage.getItem("typeUser") === "lender" ?
                 <Container className="p-2 text-center ">
                     <Button onClick ={this.handleCreateButton} variant="primary">Create a new space</Button>                 
                 </Container>:null}             
