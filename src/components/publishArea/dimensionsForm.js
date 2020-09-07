9ff32d985eb25233f5f96a9ef2b7128a74d11f9d
DONALDO\Donaldo <ldonaldo@uninorte.edu.co>
2020-09-06 19:42:32 -0500

    First form

--- src/components/publishArea/dimensionsForm.js
+++ src/components/publishArea/dimensionsForm.js
@@ -1,11 +1,13 @@
-import React, { useRef } from "react"
-import styled from "styled-components"
+import React from "react"
 import { useSelector, useDispatch } from "react-redux"
+import {Container, Form, Row, Badge, Button} from 'react-bootstrap'
 import {changeArea,
     changeLength,
     changeHeight,
     changeWidth,
     changePublishAreaView} from "../../actions/publishArea.actions"
+import { Formik } from 'formik';
+import * as Yup from "yup";    
 
 
 const base = {
@@ -20,121 +22,88 @@ const base = {
     submitId : "space_submit"
 }
 
-const FormWrapper = styled.section`
-    background: linear-gradient(180deg, #FFF9F4 1.12%, #B0CAC7 100%);
-    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25), -4px -8px 2px rgba(248, 239, 239, 0.25);
-    width: 528px;
-    height: 701px;
-    left: 74px;
-    top: 133px;
-    display: flex ;
-    justify-content: center;
-    align-content: center;
-    align-items: center;
-    font-weight: bold;
-    flex-direction: column;
-`
+const FormSchema = Yup.object().shape({  
+    width: Yup.number().typeError('Value must be a number').required("Required Field").min(1,"Value must be greater than 1").max(200),
+    length: Yup.number().typeError('Value must be a number').required("Required Field").min(1,"Value must be greater than 1").max(200),
+    height: Yup.number().typeError('Value must be a number').required("Required Field").min(0.1,"Value must be greater than 0.1").max(10)
+})
 
-const NextButton = styled.button`
-    background: #001244;
-    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25), -2px -4px 2px rgba(243, 240, 240, 0.4);
-    width: 155px;
-    height: 43px;
-    font-family: Roboto;
-    font-style: normal;
-    font-weight: bold;
-    line-height: 24px;
-    text-align: center;
-    color: #FFF9F4;
-    margin: 24px;
-    border-radius: 40px;
-`
-
-
-export default function BasicSpaceInfo (){
-
-    
+export default function BasicSpaceInfo (){    
     const width = useSelector(state => state.publishAreaReducer.width)
     const length = useSelector(state => state.publishAreaReducer.length)
     const height = useSelector(state => state.publishAreaReducer.height)
     const dispatch = useDispatch()
 
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
+    const handleSubmit = (values) => {
+        const {width, length, height} = values;
         const area = width * length
         dispatch(changeArea(area))
+        dispatch(changeWidth(width))
+        dispatch(changeHeight(height))
+        dispatch(changeLength(length))
         dispatch(changePublishAreaView(2))
         localStorage.setItem("viewState1",JSON.stringify({width, length, height}))
     }
-
-    const widthInput = useRef()
-    const lengthInput = useRef()
-    const heightInput = useRef()
     
     const HandleChange = (action, input) => {
         return e => dispatch(action(input.current.value))  
     }
 
     return(
+        <Formik 
+        initialValues = {{height:height, width: width, length: length}}
+        validationSchema = {FormSchema}
+        onSubmit = {handleSubmit} >   
+        {({
+        handleSubmit, handleChange, handleBlur, values, touched, isValid, errors, setValues
+        }) => (  
+            <>            
+            <Container>    
+                <Row className="justify-content-center">
+                    <h1>Register Dimensions</h1>         
+                </Row>
+                <Row className="justify-content-center">
+                    <Form className="justify-content-center mt-3" onSubmit={handleSubmit} noValidate>
+                        <h3>Tell us a little more about your space:</h3>
+                        <Form.Group controlId={base.widthId}>
+                            <Form.Label>Width</Form.Label>
+                            <Form.Control name="width" min={0} max={200} type="range" onChange ={handleChange} value ={values.width} />
+                            <Badge pill variant="primary">
+                            {values.width || 0}
+                            </Badge>{' '}
+                            {touched.width && errors.width ? (
+                            <div className="error-message">{errors.width}</div>
+                            ): null}
+                        </Form.Group>
+                        <Form.Group controlId={base.lengthId}>
+                            <Form.Label>Length</Form.Label>
+                            <Form.Control name="length" min={0} max={200} type="range" onChange ={handleChange} value ={values.length} />
+                            <Badge pill variant="primary">
+                            {values.length || 0}
+                            </Badge>{'mts '}
+                            {touched.length && errors.length ? (
+                            <div className="error-message">{errors.length}</div>
+                            ): null}
+                        </Form.Group>
+                        <Form.Group controlId={base.heightId}>
+                            <Form.Label>Height</Form.Label>
+                            <Form.Control name="height" min={0} max={10} step={0.1}type="range" onChange ={handleChange} value ={values.height} />
+                            <Badge pill variant="primary">
+                            {values.height || 0}
+                            </Badge>{' '}
+                            {touched.height && errors.height ? (
+                            <div className="error-message">{errors.height}</div>
+                            ): null}
+                        </Form.Group>
+                        <Button variant="primary" size="lg" type="submit">
+                            Next
+                        </Button>
+                    </Form>        
+                </Row>                     
+            </Container>
+            </>
+        )}           
+        </Formik>
         
-        <FormWrapper>
-        <h1>Tell us a little more about your space:</h1>
-        <form className = {base.formClass} onSubmit = {handleSubmit}>  
-            <label htmlFor ={base.widthId}> the width space in Mts</label>
-            <br></br>
-            <h5>{width} mts</h5>
-            <input
-                id = {base.widthId}
-                name = {base.widthId}
-                type = "range"
-                min = {0}
-                max = {200}
-                step = {1}
-                ref = {widthInput}
-                onChange = {HandleChange(changeWidth, widthInput)}
-                value = {width}
-                >
-            </input>
-            
-            <br></br>
-            <label htmlFor ={base.lengthId}> the length of your space in Mts</label>
-            <br></br>
-            <h5>{length} mts</h5>
-            <input
-                id = {base.lengthId}
-                name = {base.lengthId}
-                type = "range"
-                min = {0}
-                max = {200}
-                step = {1}
-                ref = {lengthInput}
-                onChange = {HandleChange(changeLength, lengthInput)}
-                value = {length}
-                >
-            </input>
-            
-            <br></br>
-
-            <label htmlFor ={base.heightId}> how tall is your space in Mts</label>
-            <br></br>
-            <h5>{height} mts</h5>
-            <input
-                id = {base.heightId}
-                name = {base.heightId}
-                type = "range"
-                min = {0}
-                max = {10}
-                step = {0.1}
-                ref = {heightInput}
-                onChange = {HandleChange(changeHeight, heightInput)}
-                value = {height}
-                >
-            </input>
-            <br></br>
-            <NextButton type="submit" id={base.submitId} value="submit">next</NextButton>
-
-        </form>
-        </FormWrapper>
     )
 }
