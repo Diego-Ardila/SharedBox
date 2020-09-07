3cb657c35a168e9e20af73545d179f9542423301
Diego Ardila <67027844+Diego-Ardila@users.noreply.github.com>
2020-09-07 09:56:40 -0500

    S3 admin edit photos team (#26)
    * added the page of publish area with respective components
    
    * added the dimensions and location forms components to the publish space area
    
    * all views of publish area created
    
    * creatin reducers and actions
    
    * added redux to the publishArea views
    
    * Footer and Header. General search with Home and spaces component
    
    * adding the query-string to the route
    
    * View Spaces components with redux
    
    * fixed second axios and added inital dates acording to actual date
    
    * Refactor forms with Formik
    
    * Fixing Validation
    
    * fixed the display of the login with react-bootstrap
    
    * Refactoring the viewSpaces with Formik
    
    * testing doble button solution
    
    * expand some posible solutions
    
    * Refactor register form with Formik
    
    * added multiple search forms with general query
    
    * create interfaces of diplay profile and form profile
    
    * Merge of Register and User Form
    
    * Finishing the search Spaces component with the connection with the server
    
    * fixing dates problem
    
    * creating the axios call for the photo's FormData from priceForm
    
    * fixing searchform problems
    
    * photos axios call
    
    * added lender admin area initial view
    
    * merge from admin area
    
    * solving package json merge problems
    
    * adding new changes to the admin area edit photos
    
    * updatings
    
    * Update photosEditor.js
    
    Co-authored-by: veilhelmAlex <veilhelm.alexander@gmail.com>
    Co-authored-by: DONALDO\Donaldo <ldonaldo@uninorte.edu.co>
    Co-authored-by: Diego-Martinez <d.ricardo2martinez@gmail.com>
    Co-authored-by: Veilhelm Alexander <me@veilhelmalexander.com>

--- src/components/lenderAdminArea/specificSpaceView.js
+++ src/components/lenderAdminArea/specificSpaceView.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, {useState} from "react"
 import { Row, Col } from 'react-bootstrap';
 import PhotosAdministrator from './PhotosAdministrator';
 import EditButton from "./EditButton";
 import GeneralInfoAdministrator from "./GeneralInfoAdministrator";
+import PhotosEditor from "./photosEditor"    
 import { ArrowLeftShort } from "react-bootstrap-icons"
 import styled from "styled-components"
+import {useDispatch} from 'react-redux'
+import {changePhotos} from '../../actions/publishArea.actions'
 
 const RoundedBttn = styled.button`
     cursor: pointer;
@@ -14,8 +17,13 @@ const RoundedBttn = styled.button`
     z-index:1040;
 `
 
+
+
 export default function SpecificSpaceView ({spaces, spaceId, changeViewToDisplay}){
+    const dispatch = useDispatch()
+    const [showModal,setShowModal] = useState(false)
     const renderingSpace = spaces.find( space => space._id === spaceId)
+    dispatch(changePhotos(renderingSpace.photos))
     
     return(
         <React.Fragment>
@@ -26,8 +34,9 @@ export default function SpecificSpaceView ({spaces, spaceId, changeViewToDisplay
               <Col xs={12} lg={6} md={6} className="col-6 d-inline-flex flex-column justify-content-center">
                 <h2>{renderingSpace.title}</h2>
                 <PhotosAdministrator className =" position-relative" space={renderingSpace}>
-                  <EditButton className="z-index-3"></EditButton>
+                  <EditButton onClick={()=>setShowModal(true)} className="z-index-3"></EditButton>
                 </PhotosAdministrator>
+                <PhotosEditor show={showModal} onHide={()=>setShowModal(false)}  space={renderingSpace} ></PhotosEditor>
                 <GeneralInfoAdministrator space ={renderingSpace}></GeneralInfoAdministrator>
               </Col>
               <Col xs={12} lg={6} md={6} className="col-6 d-inline-flex flex-column justify-content-center">
@@ -35,6 +44,6 @@ export default function SpecificSpaceView ({spaces, spaceId, changeViewToDisplay
                 <h1>QA</h1>
               </Col>
             </Row>
-          </React.Fragment>
+        </React.Fragment>
     )
 }
