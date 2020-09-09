c63cef2545cb7cc92974989832f579ea462611eb
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-09-09 11:37:01 -0500

    added visualization of fAQ

--- src/components/lenderAdminArea/FAQadminstrator.js
+++ src/components/lenderAdminArea/FAQadminstrator.js
@@ -1,9 +1,33 @@
 import React, { useState } from "react"
+import { Card, Accordion } from "react-bootstrap"
 
 export default function FAQadministrator({ space }) {
-    const [FAQarr, setFAQarr] = useState(space.faqs)
+    const [FAQarr] = useState( space.faqs )
 
     return (
-        <h1>test</h1>
+        <React.Fragment>
+            {(FAQarr.length === 0) ? null : (
+                <Card>
+                    <Card.Header>FAQs</Card.Header>
+                    <Card.Body>
+                    <Accordion>
+                        {FAQarr.map((faq, index) => (                            
+                                <Card>
+                                    <Card.Header>
+                                    <Accordion.Toggle as={Card.Header} variant="link" eventKey={toString(index)}>
+                                        {faq.question}
+                                    </Accordion.Toggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey={toString(index)}>
+                                     <Card.Body>{faq.answer}</Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                        ))}
+                    </Accordion>
+                    </Card.Body>
+                </Card>
+            )
+            }
+        </React.Fragment>
     )
 }
