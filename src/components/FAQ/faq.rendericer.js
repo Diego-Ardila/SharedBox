04298ed88c0deca3d29bc1e6537e2316ac270c82
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-09-10 20:44:36 -0500

    removed unncesary comments

--- src/components/FAQ/faq.rendericer.js
+++ src/components/FAQ/faq.rendericer.js
@@ -3,20 +3,6 @@ import styled from "styled-components";
 import { Container } from 'react-bootstrap';
 import { Toast } from "react-bootstrap"
 
-// const FormWrapper = styled.section`
-//     background: linear-gradient(180deg, #FFF9F4 1.12%, #B0CAC7 100%);
-//     box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25), -4px -8px 2px rgba(248, 239, 239, 0.25);
-//     width: 100%;
-//     height: 100%;
-//     left: 74px;
-//     top: 133px;
-//     display: flex ;
-//     justify-content: center;
-//     flex-direction: column;
-//     align-content: center;
-//     align-items: center;
-//     font-weight: bold;
-// `
 
 export default function Rendericer ({faqs,deleteFaq}) {
  
