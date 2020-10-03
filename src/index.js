de7bd6393ab5264cdba3f46908feb191030e0cd3
veilhelmAlex <veilhelm.alexander@gmail.com>
2020-10-02 20:16:20 -0500

    changed navBar color schema

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
 import store from "./reducers/index"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./custom.scss";
 import 'filepond/dist/filepond.min.css'
 
 
