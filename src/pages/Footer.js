3f1f9d6857afebcb949c9e0f988e5daf8c090da6
Diego <dardila90@gmail.com>
2020-10-03 22:57:39 -0500

    fixing the location form and dimensions form view

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
       <Navbar style={{height:"10vh"}} collapseOnSelect bg="primary" expand="md"  >  
         <Navbar.Toggle style={{fontSize:"1rem", border:"none", outline:"none"}}>
-        <Navbar.Brand>Make it Real Top v6</Navbar.Brand> 
+        <Navbar.Brand style={{fontSize:"1rem"}}>Make it Real Top v6</Navbar.Brand> 
         {[{name:'Diego Ardila', profile:"https://www.linkedin.com/in/diego-ardila-4a35751b0/"}, {name:'Diego Martínez', profile:"https://www.linkedin.com/in/diego-ricardo-martinez-forero/"},{name:'Donaldo Llanos', profile:"https://www.linkedin.com/in/donaldo-llanos-ter%C3%A1n/"}, {name:'Alex Guarín', profile:"https://www.linkedin.com/in/alexander-guarin-/"}].map((user) => (
               <OverlayTrigger
                 key={user.name}
